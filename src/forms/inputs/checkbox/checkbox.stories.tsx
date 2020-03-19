d61142d2c214ae5b2578c4345b5a03257032ffab
RomanAchilov <dakaktak95@gmail.com>
2020-03-19 16:18:07 +0300

    EPMDJ-2491: Update the checkbox styles
    Issue: EPMDJ-2491

--- src/forms/inputs/checkbox/checkbox.stories.tsx
+++ src/forms/inputs/checkbox/checkbox.stories.tsx
@@ -4,12 +4,27 @@ import { storiesOf } from '@storybook/react';
 import { Checkbox } from './checkbox';
 
 storiesOf('Checkbox', module).add('Checkbox', () => {
-  const [checked, setChecked] = useState(false);
+  const [checkedFirst, setCheckedFirst] = useState(false);
+  const [checkedSecond, setCheckedSecond] = useState(false);
+  const [checkedThre, setCheckedThre] = useState(false);
   return (
-    <Checkbox
-      checked={checked}
-      label="checkbox"
-      onChange={() => (checked ? setChecked(false) : setChecked(true))}
-    />
+    <>
+      <Checkbox
+        checked={checkedFirst}
+        label={checkedFirst ? 'checked' : 'not checked'}
+        onChange={() => (checkedFirst ? setCheckedFirst(false) : setCheckedFirst(true))}
+      />
+      <Checkbox
+        checked={checkedSecond}
+        label={checkedSecond ? 'checked' : 'not checked'}
+        onChange={() => (checkedSecond ? setCheckedSecond(false) : setCheckedSecond(true))}
+      />
+      <Checkbox
+        checked={checkedThre}
+        label="disable"
+        onChange={() => (checkedThre ? setCheckedThre(false) : setCheckedThre(true))}
+        disabled
+      />
+    </>
   );
 });
--- src/forms/inputs/checkbox/checkbox.tsx
+++ src/forms/inputs/checkbox/checkbox.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BEM, input } from '@redneckz/react-bem-helper';
 
-import { Icons } from '../../../components';
+import { Icons } from '../../../components/icon';
 import { CheckboxProps } from './checkbox-types';
 
 import styles from './checkbox.module.scss';
@@ -24,21 +24,22 @@ export const Checkbox = checkbox(
     return (
       <div className={className} onClick={handleOnChange}>
         <CheckboxInput name={value} checked={checked} />
-        <IconWrapper>
-          <Icons.Checkbox />
-        </IconWrapper>
-        <Label>{label}</Label>
+        <CheckboxIconWrapper>
+          <CheckMarkIcon width={10} height={7} />
+        </CheckboxIconWrapper>
+        {label && <Label>{label}</Label>}
       </div>
     );
   },
 );
 
-const CheckboxInput = checkbox.input(
+const CheckboxInput = checkbox.checkboxInput(
   input({ type: 'checkbox', checked: false, name: '' } as {
     type?: string;
     checked?: boolean;
     name?: string;
   }),
 );
-const IconWrapper = checkbox.iconWrapper('div');
+const CheckboxIconWrapper = checkbox.checkboxIconWrapper('div');
+const CheckMarkIcon = checkbox.checkMarkIcon(Icons.Check);
 const Label = checkbox.label('div');
